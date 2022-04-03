a30ed56da6dd08429cfdcac1c5a52d5f97aa4545
duanxianpi <duanxianpi@gmail.com>
2022-04-03 17:30:09 -0400

    add formating script

---
+++ src/formatting.js
@@ -0,0 +1,20 @@
+const { spawnSync } = require('child_process');
+
+const elmFomartPath = "../node_modules/.bin/";
+
+function formating(code) {
+    const elmFomartor = spawnSync("elm-format.Cmd", ['--stdin'], {input : code, cwd : elmFomartPath})
+
+    switch(elmFomartor.status) {
+        case 0:
+            return elmFomartor.stdout.toString();
+        case 1:
+            return elmFomartor.stderr.toString();
+        default:
+            return "Unkown Error!"
+    }
+
+}
+
+console.log(formating (`moduled Dom.Compsonents.Header;;`
+));
